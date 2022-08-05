c055ec451276df90685edc4a95673baba61c47a0
luccyis <mungjee08@naver.com>
2022-08-05 13:50:58 +0900

    db

--- mysql/sql/megabox.sql
+++ mysql/sql/megabox.sql
@@ -21,41 +21,34 @@ select
 	a.tdmvMovieTitle
     ,c.tdthBranch
     ,b.tdttShowTime
-from tradMovie a
-left join tradTimeTable b on b.tradMovie_tdmvSeq = a.tdmvSeq
-left join tradTheater c on c.tdthSeq = b.tradTheater_tdthSeq
-left join tradTheaterPlex d on d.tdpxSeq = b.tradTheaterPlex_tdpxSeq
+    ,d.tdpxPlexName
+from tradMovie a 
+inner join tradTimeTable b on a.tdmvSeq = b. tradMovie_tdmvSeq
+inner join tradTheater c on c.tdthSeq = b.tradTheater_tdthSeq
+inner join tradTheaterPlex d on d.tdpxSeq = b.tradTheaterPlex_tdpxSeq 
 ;
+
+
+
+
 
 -- 극장먼저
 select 
-	a.tdthBranch
-    ,c.tdmvMovieTitle
-    ,b.tdttShowTime
-from tradTheater a
-left join tradTimeTable b on b.tradTheater_tdthSeq = a.tdthSeq
-left join tradMovie c on c.tdmvSeq = b.tradMovie_tdmvSeq
+	a.tmtbSeq
+    ,a.tdttShowTime
+    b,tdthBranch
+from tradTimeTable a 
+left join tradTheater b on b.tdthSeq = a.tradTheater_tdthSeq
+left join tradMovie c on c.tdmvSeq = a.tradMovie_tdmvSeq
+left join tradTheaterPlex d on d.tdpxSeq = tradTheaterPlex_tdpxSeq
 ;
+
 
  
 
 -- 극장 목록
 
 -- 결제 페이지
-select 
-	a.tdbkPayment
-    ,b.tdttShowTime
-    ,c.tdthBranch
-    ,d.tdpxPlexName
-    ,e.thprPrice
-    ,f.tdbsRow
-    ,f.tdbsCol
-from tradBooking a
-inner join tradTimeTable b on a.tradTimeTable_tmtbSeq = b.tmtbSeq
-inner join tradTheater c on b.tradTheater_tdthSeq = c.tdthSeq
-left join tradTheaterPlex d on d.tradTheater_tdthSeq = c.tdthSeq
-left join tradTheaterPrice e on e.tradTheater_tdthSeq = c.tdthSeq
-left join tradBookingSeat f on f.tradBooking_bkSeq = a.bkSeq
-;
+
 
 
