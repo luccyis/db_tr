5cc3dbc29d653baac60702a6e006fb7db6e8cb96
luccyis <mungjee08@naver.com>
2022-08-09 13:42:00 +0900

    db

--- mysql/sql/megabox.sql
+++ mysql/sql/megabox.sql
@@ -1,5 +1,6 @@
 use nct127;
 
+
 -- 공통코드그룹
 select 
 	b.cdSeq
@@ -28,78 +29,48 @@ from tradMovie a
 order by tdmvRank
 ;
 
+-- 영화 상세 페이지
 
--- 상세 페이지
-
-
-
--- 빠른 예매 
--- 극장 먼저
-select 
-	b.tdthRegion
-	,b.tdthBranch
-    ,c.tdmvMovieTitle
-    ,a.tdttShowTime
-    ,d.tdpxPlexName
-from tradTimeTable a
-inner join tradTheater b on b.tdthSeq = a.tradTheater_tdthSeq
-inner join tradMovie c on c.tdmvSeq = a.tradMovie_tdmvSeq
-inner join tradTheaterPlex d on d.tdpxSeq = a.tradTheaterPlex_tdpxSeq
-
-;
-
---  영화 먼저
 select
-	c.tdmvMovieTitle
-    ,b.tdthBranch
-    ,a.tdttShowTime
-from tradTimeTable a
-inner join tradTheater b on b.tdthSeq = a.tradTheater_tdthSeq
-inner join tradMovie c on c.tdmvSeq = a.tradMovie_tdmvSeq
-inner join tradTheaterPlex d on d.tdpxSeq = a.tradTheaterPlex_tdpxSeq
-where tdmvMovieTitle = "탑건: 매버릭"
+	a.tdmvMovieTitle
+    ,a.tdmvTitleEng
+    ,a.tdmvAudienceScore
+    ,a.tdmvRank
+    ,a.tdmvLiked
+    ,a.tdmvAudienceNumber
+    ,a.tdmvStory
+    ,a.tdmvShowType
+    ,a.tdmvDirector
+    ,a.tdmvGenres
+    ,a.tdmvRunningTime
+    ,a.tdmvAge
+    ,a.tdmvReleaseDate
+    ,a.tdmvCast
+    
+from tradMovie a
+where a.tdmvSeq = 4
 ;
+
+-- main.무비포스트 
+ select 
+	a.tdmpId
+    ,a.tdmpImage
+    ,a.tdmpMovieTitle
+    ,a.tdmpSummary
+    ,a.tdmpTime
+ from tradMoviePost a
+ inner join tradMovie b on b.tdmvSeq = a.tradMovie_tdmvSeq
+ where a.tradMovie_tdmvSeq = 4
+ ;
+
+
+
+-- 예매. 영화 먼저
+
 
 -- 결제 페이지.좌석선택
 
-SELECT 
-	f.tdmvMovieTitle
-    ,f.tdmvShowType
-    ,g.tdthBranch
-    ,e.tdpxPlexName
-    ,d.tdttShowTime
-    ,b.tdbsRow
-    ,b.tdbsCol
-    ,b.tdbsTotalPrice
-from tradBooking a
-inner join tradBookingSeat b on b.tradBooking_tdbkSeq = a.tdbkSeq
-inner join infrMember c on c.ifmmSeq = a.infrMember_ifmmSeq
-inner join tradTimeTable d on d.tdttSeq = b.tradTimeTable_tdttSeq
-inner join tradTheaterPlex e on e.tdpxSeq = d.tradTheaterPlex_tdpxSeq
-inner join tradMovie f on f.tdmvSeq = d.tradMovie_tdmvSeq
-inner join tradTheater g on g.tdthSeq = d.tradTheater_tdthSeq
-where tdbkSeq = 1
-;
 
-
--- 결제 중 bookingPay.html 
-select
-	f.tdmvMovieTitle
-    ,f.tdmvShowType
-    ,g.tdthBranch
-    ,e.tdpxPlexName
-    ,d.tdttShowTime
-    ,b.tdbsTotalPrice
-    ,a.tdbkPayment
-from tradBooking a
-inner join tradBookingSeat b on b.tradBooking_tdbkSeq = a.tdbkSeq
-inner join infrMember c on c.ifmmSeq = a.infrMember_ifmmSeq
-inner join tradTimeTable d on d.tdttSeq = b.tradTimeTable_tdttSeq
-inner join tradTheaterPlex e on e.tdpxSeq = d.tradTheaterPlex_tdpxSeq
-inner join tradMovie f on f.tdmvSeq = d.tradMovie_tdmvSeq
-inner join tradTheater g on g.tdthSeq = d.tradTheater_tdthSeq
-where tdbkSeq = 2
-;
 
 
 
